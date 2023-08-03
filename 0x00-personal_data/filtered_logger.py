cdbeb68dc99673cec2fb2483e8642dcdeacc50d7
Martins Ndifon <martinsndifon@gmail.com>
2023-08-03 08:46:48 +0000

    correction to pycodestyle errors

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -54,12 +54,13 @@ def get_logger() -> logging.Logger:
 
     return logger
 
+
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """Connects to mysql database"""
-    DB_HOST=os.getenv('PERSONAL_DATA_DB_HOST')
-    DB_USER=os.getenv('PERSONAL_DATA_DB_USERNAME')
-    DB_PASSWORD=os.getenv('PERSONAL_DATA_DB_PASSWORD')
-    DB_NAME=os.getenv('PERSONAL_DATA_DB_NAME')
+    DB_HOST = os.getenv('PERSONAL_DATA_DB_HOST')
+    DB_USER = os.getenv('PERSONAL_DATA_DB_USERNAME')
+    DB_PASSWORD = os.getenv('PERSONAL_DATA_DB_PASSWORD')
+    DB_NAME = os.getenv('PERSONAL_DATA_DB_NAME')
 
     return mysql.connector.connect(host=DB_HOST, user=DB_USER,
                                    password=DB_PASSWORD, database=DB_NAME)
