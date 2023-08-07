844c092afc4b311da208fa06cef6a91c8438126d
Martins Ndifon <martinsndifon@gmail.com>
2023-08-07 21:31:57 +0000

    update to task-10. interacting with the json database

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,9 @@
 """Basic auth module"""
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
+from models.base import DATA
 
 
 class BasicAuth(Auth):
@@ -44,3 +47,17 @@ class BasicAuth(Auth):
             return None, None
         res = decoded_base64_authorization_header.split(':')
         return res[0], res[-1]
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        if 'User' not in DATA:
+            return None
+        users = User.search({'email': user_email})
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
