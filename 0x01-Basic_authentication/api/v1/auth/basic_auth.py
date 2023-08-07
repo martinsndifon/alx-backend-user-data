b088d1e1a7d0ba2743e40cd0cb71db9ce9129ef9
Martins Ndifon <martinsndifon@gmail.com>
2023-08-07 19:39:31 +0000

    update to task-7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,14 @@ class BasicAuth(Auth):
     def __init__(self):
         """Initialize the class"""
         super().__init__()
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        auth_val = authorization_header.split()[-1]
+        return auth_val
