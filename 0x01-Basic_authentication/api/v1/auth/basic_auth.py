d8c7cf8945e08cd25126885a0798e382d191657c
Martins Ndifon <martinsndifon@gmail.com>
2023-08-07 21:01:24 +0000

    update to task-10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,14 @@ class BasicAuth(Auth):
             return decoded_string
         except (base64.binascii.Error, TypeError):
             return None
+
+    def extract_user_credentials(self,
+            decoded_base64_authorization_header: str) -> (str, str):
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if not ':' in decoded_base64_authorization_header:
+            return None, None
+        res = decoded_base64_authorization_header.split(':')
+        return res[0], res[-1]
