a2d9402bd28f4ac0b104f7b171e3972a67e6ecd9
Martins Ndifon <martinsndifon@gmail.com>
2023-08-08 12:27:57 +0000

    pycodestyle error correction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -25,8 +25,8 @@ class BasicAuth(Auth):
         auth_val = authorization_header.split()[-1]
         return auth_val
 
-    def decode_base64_authorization_header(self,
-                                    base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
         """Decode base64 authorization header"""
         if base64_authorization_header is None:
             return None
@@ -39,14 +39,14 @@ class BasicAuth(Auth):
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
 
-    def extract_user_credentials(self,
-            decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
         """Extract user credentials from decoded auth header"""
         if decoded_base64_authorization_header is None:
             return None, None
         if type(decoded_base64_authorization_header) is not str:
             return None, None
-        if not ':' in decoded_base64_authorization_header:
+        if ':' not in decoded_base64_authorization_header:
             return None, None
         res = decoded_base64_authorization_header.split(':', 1)
         return res[0], res[-1]
