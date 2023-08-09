e8f447c17d7bb654ce4cb4b519c6f77ef17ca06e
Martins Ndifon <martinsndifon@gmail.com>
2023-08-09 17:43:19 +0000

    update to task 3, retrieve the user Id giving the session Id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,6 +13,15 @@ class SessionAuth(Auth):
         """Creates a session ID for a user with user_id"""
         if not user_id or type(user_id) is not str:
             return None
-        session_id = str(uuid.uuid4())
+        session_id: str = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(
+            self, session_id: str = None) -> Union[str, None]:
+        """Returns a User ID based on a Session ID"""
+        if not session_id or type(session_id) is not str:
+            return None
+        user_id: str = self.user_id_by_session_id.get(session_id)
+        return user_id
+
