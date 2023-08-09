1f0f200b66948693c87b3b0fbb3ef38aa89247e0
Martins Ndifon <martinsndifon@gmail.com>
2023-08-09 17:23:15 +0000

    update to task-2, creating an in memory session id cache

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-"""Session auth module"""
+"""ALX SE Backend custom session authentication module"""
 from api.v1.auth.auth import Auth
+import uuid
+from typing import Dict, Any, Union
 
 
 class SessionAuth(Auth):
-    """Session auth class"""
+    """Session auth class to manage the session auth service"""
+    user_id_by_session_id: Dict[str, Any] = {}
+
+    def create_session(self, user_id: str = None) -> Union[str, None]:
+        """Creates a session ID for a user with user_id"""
+        if not user_id or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
