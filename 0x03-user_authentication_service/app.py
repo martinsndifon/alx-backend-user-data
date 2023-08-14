aa115c7781dc10e2216b908094c74a4b4ab89df3
Martins Ndifon <martinsndifon@gmail.com>
2023-08-14 20:29:47 +0000

    update to task-7, created a route to register a new user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
 """flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"], strict_slashes=False)
-def Hello():
-    """Greet User"""
+def status():
+    """flask app status"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """Route to register a new user"""
+    data = request.form
+    email = data.get('email')
+    password = data.get('password')
+
+    try:
+        new_user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
