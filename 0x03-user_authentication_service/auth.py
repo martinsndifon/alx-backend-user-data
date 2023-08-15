d4161f6249c2a5cb110e230a3a065b0d1f0809cb
Martins Ndifon <martinsndifon@gmail.com>
2023-08-15 14:53:51 +0000

    fine tuned the response object

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,7 +15,7 @@ def _hash_password(password: str) -> bytes:
     return hashed
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """Generate an ID using the uuid module"""
     return str(uuid.uuid4())
 
